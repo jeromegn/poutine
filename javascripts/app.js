9b58e00382ed7a9e843ffec30d233c20fa4abe97
Jerome Gravel-Niquet <jeromegn@gmail.com>
2012-01-22 12:38:42 -0500

    getting there

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,89 +1,154 @@
 (function() {
-  var bonzo, decode64, highlight_code, keyStr, qwery, render_content, render_navigation;
+  var decode64, keyStr;
 
-  bonzo = require("bonzo");
+  window.DocumentUp = (function() {
 
-  qwery = require("qwery");
+    function DocumentUp() {}
 
-  render_navigation = function() {
-    var $sections, current_section, current_subsection;
-    current_section = 0;
-    current_subsection = 0;
-    $sections = $("#sections");
-    $sections.empty();
-    return $("h1, h2, h3, h4, h5, h6").each(function(el) {
-      var $subsection, section_id;
-      if (el.tagName === "H2") {
-        current_subsection = 0;
-        current_section++;
-        el.id = section_id = "section-" + current_section;
-        return $sections.append("<li id=\"for-" + section_id + "\">\n  <a href=\"#" + section_id + "\">" + el.textContent + "</a>\n</li>");
-      } else if (el.tagName === "H3") {
-        current_subsection++;
-        el.id = section_id = "section-" + current_section + "-" + current_subsection;
-        $subsection = $("#for-section-" + current_section + " ul");
-        if (!$subsection.length) {
-          $("#for-section-" + current_section).append("<ul></ul>");
+    DocumentUp.template = function(locals) {
+      return "<nav id=\"nav\">\n  <header>\n    <a href=\"#\" id=\"logo\">" + locals.name + "</a>\n  </header>\n  <ul id=\"sections\">\n  </ul>\n</nav>\n<div id=\"content\">\n  <div id=\"loader\">\n    Loading documentation...\n  </div>\n</div>";
+    };
+
+    DocumentUp.defaults = {
+      color: "#369",
+      twitter: null,
+      changelog: false,
+      travis: false
+    };
+
+    DocumentUp.document = function(options) {
+      var key, repo, value, _base, _ref;
+      var _this = this;
+      this.options = options;
+      if ("string" === typeof this.options) {
+        repo = this.options;
+        this.options = {
+          repo: repo
+        };
+      }
+      if (!this.options || !this.options.repo || !/\//.test(this.options.repo)) {
+        throw new Error("Repository required with format: username/repository");
+      }
+      _ref = this.defaults;
+      for (key in _ref) {
+        value = _ref[key];
+        if (!this.options[key]) this.options[key] = value;
+      }
+      (_base = this.options).name || (_base.name = this.options.repo.replace(/.+\//, ""));
+      $.domReady(function() {
+        var $nav, extra, iframe, twitter, _i, _len, _ref2, _results;
+        $("body").html(_this.template(_this.options));
+        $("head").append("<style type=\"text/css\">\n  a {color: " + _this.options.color + "}\n</style>");
+        $nav = $("#nav");
+        if (_this.options.travis) {
+          $nav.append("<div id=\"travis\" class=\"extra\">\n  <a href=\"http://travis-ci.org/" + _this.options.repo + "\">Travis CI</a>\n  <a href=\"http://travis-ci.org/" + _this.options.repo + "\">\n    <img src=\"https://secure.travis-ci.org/" + _this.options.repo + ".png\">\n  </a>\n</div>");
+        }
+        if (_this.options.twitter) {
+          if (!(_this.options.twitter instanceof Array)) {
+            _this.options.twitter = [_this.options.twitter];
+          }
+          _ref2 = _this.options.twitter;
+          _results = [];
+          for (_i = 0, _len = _ref2.length; _i < _len; _i++) {
+            twitter = _ref2[_i];
+            twitter = twitter.replace("@", "");
+            extra = $("<div class='extra twitter'>");
+            iframe = $('<iframe allowtransparency="true" frameborder="0" scrolling="no" style="width:162px; height:20px;">');
+            iframe.attr("src", "https://platform.twitter.com/widgets/follow_button.html?screen_name=" + twitter + "&show_count=false");
+            extra.append(iframe);
+            _results.push($nav.append(extra));
+          }
+          return _results;
+        }
+      });
+      return this.getReadme(function(err, html) {
+        _this.html = html;
+        if (err) throw err;
+        return $.domReady(function() {
+          return _this.renderContent();
+        });
+      });
+    };
+
+    DocumentUp.getReadme = function(callback) {
+      var html, using_cache;
+      var _this = this;
+      using_cache = false;
+      if (html = localStorage.getItem("cached_content")) {
+        callback(null, html);
+        this.usingCache = true;
+      }
+      return $.ajax({
+        url: "https://api.github.com/repos/" + this.options.repo + "/git/trees/master?callback=?",
+        type: "jsonp",
+        success: function(resp) {
+          var last_sha, obj, readme_sha, _i, _len, _ref;
+          _ref = resp.data.tree;
+          for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+            obj = _ref[_i];
+            if (/readme/i.test(obj.path)) readme_sha = obj.sha;
+          }
+          last_sha = localStorage.getItem("readme_sha");
+          if (readme_sha !== last_sha) {
+            return $.ajax({
+              url: "https://api.github.com/repos/" + _this.options.repo + "/git/blobs/" + readme_sha + "?callback=?",
+              type: "jsonp",
+              success: function(resp) {
+                html = marked(decode64(resp.data.content));
+                localStorage.setItem("cached_content", html);
+                localStorage.setItem("readme_sha", readme_sha);
+                if (!_this.usingCache) return callback(null, html);
+                return $.domReady(function() {
+                  var refresh_link;
+                  var _this = this;
+                  refresh_link = $("<a id='refresh' href='#'>There's a new version of the documentation<br>Click here or refresh to see it.</a>");
+                  $("body").append(refresh_link);
+                  return refresh_link.bind("click", function(event) {
+                    event.preventDefault();
+                    callback(null, html);
+                    return refresh_link.remove();
+                  });
+                });
+              }
+            });
+          }
+        }
+      });
+    };
+
+    DocumentUp.renderContent = function() {
+      var $sections, current_section, current_subsection;
+      $("#content").html(this.html);
+      current_section = 0;
+      current_subsection = 0;
+      $sections = $("#sections");
+      $sections.empty();
+      $("h1, h2, h3, h4, h5, h6").each(function(el) {
+        var $subsection, section_id;
+        if (el.tagName === "H2") {
+          current_subsection = 0;
+          current_section++;
+          el.id = section_id = "section-" + current_section;
+          return $sections.append("<li id=\"for-" + section_id + "\">\n  <a href=\"#" + section_id + "\">" + el.textContent + "</a>\n</li>");
+        } else if (el.tagName === "H3") {
+          current_subsection++;
+          el.id = section_id = "section-" + current_section + "-" + current_subsection;
           $subsection = $("#for-section-" + current_section + " ul");
+          if (!$subsection.length) {
+            $("#for-section-" + current_section).append("<ul></ul>");
+            $subsection = $("#for-section-" + current_section + " ul");
+          }
+          return $subsection.append("<li id=\"for-" + section_id + "\">\n  <a href=\"#" + section_id + "\">" + el.textContent + "</a>\n</li>");
         }
-        return $subsection.append("<li id=\"for-" + section_id + "\">\n  <a href=\"#" + section_id + "\">" + el.textContent + "</a>\n</li>");
-      }
-    });
-  };
+      });
+      return $("pre code").each(function(el) {
+        return hljs.initHighlighting(el);
+      });
+    };
 
-  highlight_code = function() {
-    return $("pre code").each(function(el) {
-      return hljs.initHighlighting(el);
-    });
-  };
+    return DocumentUp;
 
-  render_content = function(html) {
-    $("#content").html(html);
-    render_navigation();
-    return highlight_code();
-  };
-
-  $.domReady(function() {
-    var cached, using_cache;
-    using_cache = false;
-    if (cached = localStorage.getItem("cached")) {
-      render_content(cached);
-      using_cache = true;
-    }
-    return $.ajax({
-      url: "https://api.github.com/repos/jeromegn/poutine/git/trees/master?callback=?",
-      type: "jsonp",
-      success: function(resp) {
-        var last_sha, obj, readme_sha, _i, _len, _ref;
-        _ref = resp.data.tree;
-        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-          obj = _ref[_i];
-          if (obj.path === "README.md") readme_sha = obj.sha;
-        }
-        last_sha = localStorage.getItem("last_sha");
-        if (readme_sha !== last_sha) {
-          return $.ajax({
-            url: "https://api.github.com/repos/jeromegn/poutine/git/blobs/" + readme_sha + "?callback=?",
-            type: "jsonp",
-            success: function(resp) {
-              var content, refresh_link;
-              content = marked(decode64(resp.data.content));
-              localStorage.setItem("cached", content);
-              localStorage.setItem("last_sha", readme_sha);
-              if (!using_cache) return render_content(content);
-              refresh_link = $("<a id='refresh' href='#'>There's a new version of the documentation<br>Click here or refresh to see it.</a>");
-              $("body").append(refresh_link);
-              return refresh_link.bind("click", function(event) {
-                event.preventDefault();
-                render_content(content);
-                return refresh_link.remove();
-              });
-            }
-          });
-        }
-      }
-    });
-  });
+  })();
 
   decode64 = function(input) {
     var chr1, chr2, chr3, enc1, enc2, enc3, enc4, i, output;
--- src/javascripts/app.coffee
+++ src/javascripts/app.coffee
@@ -1,77 +1,149 @@
-bonzo = require("bonzo")
-qwery = require("qwery")
+class window.DocumentUp
+  @template = (locals)->
+    """
+    <nav id="nav">
+      <header>
+        <a href="#" id="logo">#{locals.name}</a>
+      </header>
+      <ul id="sections">
+      </ul>
+    </nav>
+    <div id="content">
+      <div id="loader">
+        Loading documentation...
+      </div>
+    </div>
+    """
 
-render_navigation = ->
-  current_section = 0
-  current_subsection = 0
+  @defaults =
+    color: "#369"
+    twitter: null
+    changelog: false
+    travis: false
 
-  $sections = $("#sections")
+  @document = (@options)->
+    if "string" == typeof @options
+      repo = @options
+      @options = 
+        repo: repo
+    
+    if !@options or !@options.repo or !/\//.test(@options.repo)
+      throw new Error("Repository required with format: username/repository")
 
-  $sections.empty()
+    @options[key] = value for key, value of @defaults when !@options[key]
 
-  $("h1, h2, h3, h4, h5, h6").each (el)->
-    if el.tagName == "H2"
-      current_subsection = 0
-      current_section++
-      el.id = section_id = "section-#{current_section}"
-      $sections.append """
-        <li id="for-#{section_id}">
-          <a href="##{section_id}">#{el.textContent}</a>
-        </li>
+    @options.name ||= @options.repo.replace(/.+\//, "")
+
+    # Prepare layout
+    $.domReady =>
+      $("body").html @template(@options)
+      $("head").append """
+        <style type="text/css">
+          a {color: #{@options.color}}
+        </style>
       """
-    else if el.tagName == "H3"
-      current_subsection++
-      el.id = section_id = "section-#{current_section}-#{current_subsection}"
-      $subsection = $("#for-section-#{current_section} ul")
-      unless $subsection.length
-        $("#for-section-#{current_section}").append("<ul></ul>")
-        $subsection = $("#for-section-#{current_section} ul")
-      $subsection.append """
-        <li id="for-#{section_id}">
-          <a href="##{section_id}">#{el.textContent}</a>
-        </li>
-      """
 
-highlight_code = ->
-  $("pre code").each (el)->
-    hljs.initHighlighting(el)
+      $nav = $("#nav")
 
-render_content = (html)->
-  $("#content").html html
-  render_navigation()
-  highlight_code()
-
-$.domReady ->
-  using_cache = false
-  
-  if cached = localStorage.getItem("cached")
-    render_content(cached)
-    using_cache = true
-
-  $.ajax
-    url: "https://api.github.com/repos/jeromegn/poutine/git/trees/master?callback=?"
-    type: "jsonp"
-    success: (resp)->
-      readme_sha = obj.sha for obj in resp.data.tree when obj.path == "README.md"
-      last_sha = localStorage.getItem("last_sha")
+      if @options.travis
+        $nav.append """
+          <div id="travis" class="extra">
+            <a href="http://travis-ci.org/#{@options.repo}">Travis CI</a>
+            <a href="http://travis-ci.org/#{@options.repo}">
+              <img src="https://secure.travis-ci.org/#{@options.repo}.png">
+            </a>
+          </div>
+        """
       
-      if readme_sha != last_sha
-        $.ajax
-          url: "https://api.github.com/repos/jeromegn/poutine/git/blobs/#{readme_sha}?callback=?"
-          type: "jsonp"
-          success: (resp)->
-            content = marked(decode64(resp.data.content))
-            localStorage.setItem("cached", content)
-            localStorage.setItem("last_sha", readme_sha)
-            
-            return render_content(content) unless using_cache
+      if @options.twitter
+        @options.twitter = [@options.twitter] unless @options.twitter instanceof Array
+
+        for twitter in @options.twitter
+          twitter = twitter.replace("@", "")
+          extra = $("<div class='extra twitter'>")
+          iframe = $('<iframe allowtransparency="true" frameborder="0" scrolling="no" style="width:162px; height:20px;">')
+          iframe.attr "src", "https://platform.twitter.com/widgets/follow_button.html?screen_name=#{twitter}&show_count=false"
+          extra.append(iframe)
+          $nav.append extra
+
+    @getReadme (err, @html)=>
+      return throw err if err
+      $.domReady =>
+        @renderContent()
+  
+  @getReadme = (callback)->
+    using_cache = false
+  
+    if html = localStorage.getItem("cached_content")
+      callback(null, html)
+      @usingCache = true
+
+    $.ajax
+      url: "https://api.github.com/repos/#{@options.repo}/git/trees/master?callback=?"
+      type: "jsonp"
+      success: (resp)=>
+        readme_sha = obj.sha for obj in resp.data.tree when /readme/i.test(obj.path)
+        last_sha = localStorage.getItem("readme_sha")
+        if readme_sha != last_sha
+          $.ajax
+            url: "https://api.github.com/repos/#{@options.repo}/git/blobs/#{readme_sha}?callback=?"
+            type: "jsonp"
+            success: (resp)=>
+              html = marked(decode64(resp.data.content))
+              localStorage.setItem("cached_content", html)
+              localStorage.setItem("readme_sha", readme_sha)
               
-            refresh_link = $("<a id='refresh' href='#'>There's a new version of the documentation<br>Click here or refresh to see it.</a>")
-            $("body").append(refresh_link)
-            refresh_link.bind "click", (event)->
-              event.preventDefault()
-              render_content(content)
-              refresh_link.remove()
+              return callback(null, html) unless @usingCache
+              
+              $.domReady ->
+                # Show a link to tell the user to refresh his browser to get
+                # the latest version of the readme  
+                refresh_link = $("<a id='refresh' href='#'>There's a new version of the documentation<br>Click here or refresh to see it.</a>")
+                $("body").append(refresh_link)
+                refresh_link.bind "click", (event)=>
+                  event.preventDefault()
+                  callback(null, html)
+                  refresh_link.remove()
+  
+  @renderContent = ->
+    # Populate HTML content
+    $("#content").html @html
+
+    # Generate the navigation tree with the document structure
+    current_section = 0
+    current_subsection = 0
+
+    $sections = $("#sections")
+
+    $sections.empty()
+
+    $("h1, h2, h3, h4, h5, h6").each (el)->
+      if el.tagName == "H2"
+        current_subsection = 0
+        current_section++
+        el.id = section_id = "section-#{current_section}"
+        $sections.append """
+          <li id="for-#{section_id}">
+            <a href="##{section_id}">#{el.textContent}</a>
+          </li>
+        """
+      else if el.tagName == "H3"
+        current_subsection++
+        el.id = section_id = "section-#{current_section}-#{current_subsection}"
+        $subsection = $("#for-section-#{current_section} ul")
+        unless $subsection.length
+          $("#for-section-#{current_section}").append("<ul></ul>")
+          $subsection = $("#for-section-#{current_section} ul")
+        $subsection.append """
+          <li id="for-#{section_id}">
+            <a href="##{section_id}">#{el.textContent}</a>
+          </li>
+        """
+    
+    # Highlight the code bits:
+    $("pre code").each (el)->
+      hljs.initHighlighting(el)
+
 
 decode64 = (input) ->
   output = ""
